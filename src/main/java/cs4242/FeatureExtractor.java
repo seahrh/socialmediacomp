108b4803624360e64e1c3e68c5015ae059e8663c
Ruhong <seahrh@gmail.com>
2015-02-18 16:36:10 +0800

    Model file can be saved and loaded

--- src/main/java/cs4242/FeatureExtractor.java
+++ src/main/java/cs4242/FeatureExtractor.java
@@ -2,17 +2,23 @@ package cs4242;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import weka.classifiers.Classifier;
 import weka.classifiers.functions.LibSVM;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.SerializationHelper;
 import weka.core.converters.ArffSaver;
+import weka.core.stopwords.WordsFromFile;
 import weka.core.tokenizers.CharacterDelimitedTokenizer;
 import weka.core.tokenizers.Tokenizer;
 import weka.core.tokenizers.WordTokenizer;
@@ -28,9 +34,9 @@ import com.google.common.io.Files;
 
 public class FeatureExtractor {
 
-	public static final ImmutableMap<String, Integer> CLASSES = ImmutableMap
-			.<String, Integer> builder().put("positive", 1).put("negative", -1)
-			.put("neutral", 0).build();
+	public static final ImmutableMap<String, String> CLASSES = ImmutableMap
+			.<String, String> builder().put("positive", "1")
+			.put("negative", "-1").put("neutral", "0").build();
 
 	public static final ImmutableList<String> INPUT_FIELDS = ImmutableList
 			.<String> builder()
@@ -40,8 +46,29 @@ public class FeatureExtractor {
 	public static final ArrayList<Attribute> ATTRIBUTES = new ArrayList<Attribute>();
 
 	static {
-		List<String> values = null;
-		ATTRIBUTES.add(new Attribute("classlabel"));
+		// Class label attribute is nominal
+
+		// There is a known problem saving SparseInstance objects from datasets
+		// that have string attributes. In Weka, string and nominal data values
+		// are stored as numbers; these numbers act as indexes into an array of
+		// possible attribute values (this is very efficient). However, the
+		// first string value is assigned index 0: this means that, internally,
+		// this value is stored as a 0. When a SparseInstance is written, string
+		// instances with internal value 0 are not output, so their string value
+		// is lost (and when the arff file is read again, the default value 0 is
+		// the index of a different string value, so the attribute value appears
+		// to change). To get around this problem, add a dummy string value at
+		// index 0 that is never used whenever you declare string attributes
+		// that are likely to be used in SparseInstance objects and saved as
+		// Sparse ARFF files.
+
+		List<String> values = ImmutableList.<String> builder()
+				.add("dummy", "1", "0", "-1").build();
+		ATTRIBUTES.add(new Attribute("classlabel", values));
+
+		// Text attribute is a string
+
+		values = null;
 		ATTRIBUTES.add(new Attribute("text", values));
 	}
 
@@ -49,8 +76,8 @@ public class FeatureExtractor {
 
 	public static void main(String[] args) throws IOException {
 
-		if (args.length != 1) {
-			System.out.println("Usage: FeatureExtractor <input.tsv>");
+		if (args.length != 3) {
+			System.out.println("Usage: FeatureExtractor <input> <stopwords> <output model>");
 			System.exit(1);
 		}
 
@@ -58,17 +85,22 @@ public class FeatureExtractor {
 		String inFilename = Files.getNameWithoutExtension(inFilePath);
 		File inFile = new File(inFilePath);
 
+		String stopwordsFilePath = args[1];
+		String modelFilePath = args[2];
+
 		String parentDir = inFile.getParent();
-		BufferedReader br = new BufferedReader(new FileReader(inFile));
+		BufferedReader br = null;
 		String line;
 		String sentiment;
-		Optional<Integer> classValue;
+		Optional<String> classValue;
 		List<String> row;
 		int count = 0;
 		Instances data = initDataset(inFilename);
 		Instance inst;
 
 		try {
+
+			br = new BufferedReader(new FileReader(inFile));
 
 			// Skip the header row
 
@@ -90,6 +122,9 @@ public class FeatureExtractor {
 				} else {
 					continue;
 				}
+				
+				//TODO Normalize the string before tokenizing
+				
 				inst.setValue(ATTRIBUTES.get(1),
 						row.get(INPUT_FIELDS.indexOf("content")));
 				data.add(inst);
@@ -99,13 +134,15 @@ public class FeatureExtractor {
 
 			System.out.printf("Processed %s rows\n", count);
 
-			data = stringToWordVector(data);
+			data = stringToWordVector(data, stopwordsFilePath);
 
 			saveArff(data, outFilePath(parentDir, inFilename));
 			System.out.printf("Saved %s attributes and %s instances\n",
 					data.numAttributes(), data.size());
 
-			// svm(data);
+			svm(data, modelFilePath);
+			
+			loadModel(modelFilePath);
 
 		} catch (Exception e) {
 
@@ -124,34 +161,56 @@ public class FeatureExtractor {
 		return data;
 	}
 
-	private static Instances stringToWordVector(Instances in) throws Exception {
+	private static Instances stringToWordVector(Instances in,
+			String stopwordsFilePath) throws Exception {
 		StringToWordVector filter = new StringToWordVector();
 		// filter.setOptions(options);
 		filter.setLowerCaseTokens(true);
 		filter.setInputFormat(in);
-		
-		WordTokenizer tokenizer = new WordTokenizer();
-		
-		// Removed exclamation mark, single quote (negation words)
-		// 
-		
-		tokenizer.setDelimiters(" \r\n\t.,;:\"()?/\\&=%^~`|{}[]-1234567890");
-		
-		filter.setTokenizer(tokenizer);
-		//filter.set
+
+		filter.setTokenizer(tokenizer());
+		filter.setStopwordsHandler(stopwords(stopwordsFilePath));
+		filter.setAttributeIndices("2");
+
 		Instances out = Filter.useFilter(in, filter);
 
 		return out;
 	}
 
-	private static void svm(Instances data) throws Exception {
-		// initialize svm classifier
-		LibSVM svm = new LibSVM();
-		svm.buildClassifier(data);
+	private static WordTokenizer tokenizer() {
+		WordTokenizer tokenizer = new WordTokenizer();
+
+		// Removed exclamation mark, single quote (negation words)
+		// TODO emoticons
+
+		tokenizer.setDelimiters(" \r\n\t.,;:\"()?/\\&=%^~`|{}[]-1234567890");
+		return tokenizer;
 	}
 
-	private static Optional<Integer> classValue(String className) {
-		Integer classValue = CLASSES.get(className);
+	private static WordsFromFile stopwords(String path) throws IOException {
+		WordsFromFile stopwords = new WordsFromFile();
+		stopwords.setStopwords(new File(path));
+		return stopwords;
+
+	}
+
+	private static void svm(Instances data, String modelPath) throws Exception {
+		// initialize svm classifier
+		LibSVM svm = new LibSVM();
+		//svm.setModelFile(new File(modelPath));
+		svm.buildClassifier(data);
+		SerializationHelper.write(modelPath, svm);
+		System.out.printf("SVM classifier trained\nModel file saved: %s\n", modelPath);
+	}
+	
+	private static LibSVM loadModel(String path) throws Exception {
+		LibSVM svm = (LibSVM)SerializationHelper.read(path);
+		System.out.println("SVM classifer loaded from file successfully.");
+		return svm;
+	}
+
+	private static Optional<String> classValue(String className) {
+		String classValue = CLASSES.get(className);
 		if (classValue != null) {
 			return Optional.of(classValue);
 		}
