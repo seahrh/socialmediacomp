54dc208df0e61233f01811d70a5a9bdc9e08ed1a
Ruhong <seahrh@gmail.com>
2015-04-12 16:07:52 +0800

    New package for A3, duplicate id detection

---
+++ src/main/java/cs4242/a3/StringUtil.java
@@ -0,0 +1,23 @@
+package cs4242.a3;
+
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Strings;
+
+public final class StringUtil {
+	
+	private static final CharMatcher CONTROL_CHARACTERS = CharMatcher.anyOf("\t\b\n\r\f");
+	
+
+	private StringUtil() {
+		// Private constructor, not meant to be instantiated
+	}
+		
+	public static String stripControlCharacters(String s) {
+		return CONTROL_CHARACTERS.removeFrom(s);
+	}
+	
+	public static String trim(String s) {
+		s = Strings.nullToEmpty(s);
+		return CharMatcher.WHITESPACE.trimFrom(s);
+	}
+}
