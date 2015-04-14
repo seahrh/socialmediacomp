ea9ce128b8f7236336ee2cc51d031806b4e97fc1
Ruhong <seahrh@gmail.com>
2015-04-14 13:45:06 +0800

    A3 merge arff (untested)

--- src/main/java/cs4242/a3/ArffGenerator.java
+++ src/main/java/cs4242/a3/ArffGenerator.java
@@ -15,7 +15,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import weka.core.Attribute;
+import weka.core.DenseInstance;
 import weka.core.Instances;
+import weka.core.SparseInstance;
+import weka.core.converters.ArffLoader;
 import weka.core.converters.ArffSaver;
 
 import com.google.common.base.CharMatcher;
@@ -34,12 +38,15 @@ public final class ArffGenerator {
 
 		List<FeatureVector> fvs = null;
 		Instances data = null;
+		Instances other = null;
 		long startTime = System.currentTimeMillis();
 
 		try {
 
 			fvs = extract();
 			data = FeatureVector.getInstances(fvs, ids);
+			other = loadArff(System.getProperty("a3.input.crtdt.file.path"));
+			data = leftJoin(data, other, "id");
 			saveArff(data);
 
 		} catch (Exception e) {
@@ -131,8 +138,85 @@ public final class ArffGenerator {
 
 		return data;
 	}
+	
+	public static Instances leftJoin(Instances left, Instances right, String predicateName) {
+		final int LEFT_NUMBER_OF_ATTRIBUTES = left.numAttributes();
+		final int RIGHT_NUMBER_OF_ATTRIBUTES = right.numAttributes();
+		final int LEFT_NUMBER_OF_INSTANCES = left.size();
+		final int RIGHT_NUMBER_OF_INSTANCES = right.size();
+		final String RELATION_NAME = left.relationName();
+		final String CLASS_NAME = left.classAttribute().name();
+		Attribute attr;
+		String aname;
+		ArrayList<Attribute> attrs = new ArrayList<Attribute>(LEFT_NUMBER_OF_ATTRIBUTES + RIGHT_NUMBER_OF_ATTRIBUTES);
+		ArrayList<String> mergeAttributeNames = new ArrayList<String>(RIGHT_NUMBER_OF_ATTRIBUTES);
+		SparseInstance joined;
+		SparseInstance li;
+		DenseInstance ri;
+		double lpred;
+		double rpred;
+		double val;
+		boolean matched = false;
+		
+		for (int i = 0; i < LEFT_NUMBER_OF_ATTRIBUTES; i++) {
+			attrs.add(left.attribute(i));
+		}
+		
+		// Skip the first two attributes, which are always class and id
+		
+		for (int i = 2; i < RIGHT_NUMBER_OF_ATTRIBUTES; i++) {
+			attr = right.attribute(i);
+			aname = attr.name();
+			attrs.add(attr);
+			mergeAttributeNames.add(aname);
+			System.out.println(aname);
+		}
+		
+		Instances result = new Instances(RELATION_NAME, attrs, LEFT_NUMBER_OF_INSTANCES + RIGHT_NUMBER_OF_INSTANCES);
+		
+		for (int i = 0; i < LEFT_NUMBER_OF_INSTANCES; i++) {
+			
+			matched = false;
+			joined = new SparseInstance(1);
+			joined.setDataset(result);
+			
+			li = (SparseInstance) left.instance(i);
+			joined = (SparseInstance) joined.mergeInstance(li);
+			lpred = li.value(result.attribute(predicateName));
+			
+			for (int j = 0; j < RIGHT_NUMBER_OF_INSTANCES; j++) {
+				
+				ri = (DenseInstance) right.instance(i);
+				rpred = ri.value(right.attribute(predicateName));
+				
+				if (lpred == rpred) {
+					// Matching predicate, add attributes to merge
+					
+					for (String name : mergeAttributeNames) {
+						
+						val = ri.value(right.attribute(name));
+						joined.setValue(result.attribute(name), val);
+					}
+					
+					matched = true;
+				}
+			}
+			
+			if (!matched) {
+				for (String name : mergeAttributeNames) {
+					joined.setMissing(result.attribute(name));
+				}
+			}
+			
+			result.add(joined);
+		}
+		
+		result.setClass(result.attribute(CLASS_NAME));
+		
+		return result;
+	}
 
-	private static void saveArff(Instances data) throws IOException {
+	public static void saveArff(Instances data) throws IOException {
 		ArffSaver arff = new ArffSaver();
 		String filePath = System.getProperty("output.file.path");
 		arff.setInstances(data);
@@ -142,6 +226,13 @@ public final class ArffGenerator {
 		System.out.printf("Saved %s attributes and %s instances:\n\t%s\n",
 				data.numAttributes(), data.size(), filePath);
 
+	}
+	
+	public static Instances loadArff(String filePath) throws IOException {
+		ArffLoader loader = new ArffLoader();
+		loader.setSource(new File(filePath));
+		Instances data = loader.getDataSet();
+		return data;
 	}
 
 }
